e4fd05b09a514886c9db5e21ee6422aea820de23
Srinevasan Sothynathan <37037333-sothynathans@users.noreply.replit.com>
2024-10-08 11:44:22 +0000

    Moved Participants header above Add Participant card

--- main.py
+++ main.py
@@ -139,6 +139,6 @@ def remove_expense(index):
     save_data()
     return redirect(url_for('index'))
 
-# Run the app - i'm restarting this, ANOTHER ONE
+# Run the app
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)

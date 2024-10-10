7092237644fa729eb410bf266e531b8b0943202c
Srinevasan Sothynathan <37037333-sothynathans@users.noreply.replit.com>
2024-10-10 12:19:29 +0000

    Commit test 15

--- main.py
+++ main.py
@@ -171,6 +171,6 @@ def remove_expense(index):
     save_data()
     return redirect(url_for('index'))
 
-# Run the app  -----------------  Commit Test 14
+# Run the app  -----------------  Commit Test 15
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

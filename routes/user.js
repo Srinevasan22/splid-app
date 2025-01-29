9c0a02f59348cafa1a362fdd9c56b8679704de2f
Srinevasan22 <sothynathans@gmail.com>
2025-01-29 16:17:03 +0000

    29.01.2025 Commit 6

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/userController');
+const userController = require('../controllers/userController.js');
 const router = express.Router();
 
 // Correct route path without /splid prefix
